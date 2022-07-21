e125054d74b63cc571466f270444b2f1eb19f359
LForchini <leonardo.forchini@gmail.com>
2022-07-21 10:35:29 +0100

    Added basic program structure + animation

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var config = {
+const CELL_WIDTH = 24;
+const CELL_HEIGHT = 24;
+
+const WIDTH = 20;
+const HEIGHT = 20;
+
+const PIXEL_WIDTH = CELL_WIDTH * WIDTH;
+const PIXEL_HEIGHT = CELL_HEIGHT * HEIGHT;
+
+let config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: PIXEL_WIDTH,
+  height: PIXEL_HEIGHT,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -9,10 +25,46 @@ var config = {
   },
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-function preload() {}
+let maze;
 
-function create() {}
+function preload() {
+  this.load.spritesheet("tileset", "assets/tileset.png", {
+    frameWidth: 16,
+    frameHeight: 16,
+  });
 
-function update() {}
+  this.load.spritesheet("player", "assets/player.png", {
+    frameWidth: 24,
+    frameHeight: 24,
+  });
+}
+
+function create() {
+  // Sprite init
+
+  let player = this.physics.add.sprite(CELL_WIDTH, CELL_HEIGHT, "player");
+
+  this.anims.create({
+    key: "idle",
+    frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+    frameRate: 8,
+    repeat: -1,
+  });
+
+  player.anims.play("idle", true);
+
+  // Maze init
+  maze = generateMaze();
+
+  // Place walls in the pattern in maze
+}
+
+function update() {
+  // Player control stuff
+}
+
+function generateMaze() {
+  // TODO
+}
